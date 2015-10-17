361e995c168fa4477d097bc90745a92826d7e8bf
ant888 <anewhope8@outlook.com>
2015-10-17 15:39:15 -0400

    Create main.c
    Nothing here...

---
+++ main.c
@@ -0,0 +1,52 @@
+#include <3ds.h>
+#include <stdio.h>
+
+
+int main(int argc, char **argv)
+{
+	// Initialize services
+	gfxInitDefault();
+
+	//Initialize console on top screen. Using NULL as the second argument tells the console library to use the internal console structure as current one
+	consoleInit(GFX_TOP, NULL);
+
+	printf("\x1b[29;15HPress Start to exit.");
+
+	// Main loop
+	while (aptMainLoop())
+	{
+		//Scan all the inputs. This should be done once for each frame
+		hidScanInput();
+
+		//hidKeysDown returns information about which buttons have been just pressed (and they weren't in the previous frame)
+		u32 kDown = hidKeysDown();
+
+		if (kDown & KEY_START) break; // break in order to return to hbmenu
+
+		//Print current time
+		time_t unixTime = time(NULL);
+		struct tm* timeStruct = gmtime((const time_t *)&unixTime);
+
+		int hours = timeStruct->tm_hour;
+		int minutes = timeStruct->tm_min;
+		int seconds = timeStruct->tm_sec;
+		int day = timeStruct->tm_mday;
+		int month = timeStruct->tm_mon;
+		int year = timeStruct->tm_year +1900;
+
+		printf("\x1b[Hello, World!", hours, minutes, seconds);
+		printf("\x1b[Hello, Earth!", day, month, year);
+
+		// Flush and swap framebuffers
+		gfxFlushBuffers();
+		gfxSwapBuffers();
+
+		//Wait for VBlank
+		gspWaitForVBlank();
+	}
+
+	// Exit services
+	gfxExit();
+
+	return 0;
+}
